352f41ed40eb840bc73566051d09535ac4a2306c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-29 11:22:09 +0100

    Create generated directory unless it is already created

--- src/derive/mod.rs
+++ src/derive/mod.rs
@@ -1,14 +1,17 @@
 extern crate syn;
 
 use std::env;
-use std::fs::File;
+use std::fs::{File, create_dir};
 use std::io::{Write, BufWriter};
 use syn::DeriveInput;
 
 pub fn generate_elm(ast: &DeriveInput) {
     let mut path = env::current_dir().unwrap();
+    path.push("generated");
+    if path.exists() && path.is_file() { panic!("expecting \"generated\" to be a directory"); }
+    if !path.exists() { create_dir(path.clone()).expect("problem creating \"generated\" directory") }
     let name = &ast.ident;
-    path.push(format!("generated/{}.elm", name));
+    path.push(format!("{}.elm", name));
 
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
