f19563051bc99e4ea681a18dc639ee981b0283c1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-29 08:37:00 +0100

    Move proc_macro definition to root of lib

--- src/derive/mod.rs
+++ src/derive/mod.rs
@@ -1,20 +1,7 @@
-extern crate proc_macro;
-
 extern crate syn;
 
-use proc_macro::TokenStream;
+use syn::DeriveInput;
 
-#[proc_macro_derive(Elm)]
-pub fn generate_elm(input: TokenStream) -> TokenStream {
-    let source = input.to_string();
-
-    let ast = syn::parse_derive_input(&source).unwrap();
-
-    generate_elm(&ast);
-
-    input
-}
-
-fn generate_elm(ast: &syn::DeriveInput) {
+pub fn generate_elm_for(_: &DeriveInput) {
     // do nothing
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,21 @@
+extern crate proc_macro;
+extern crate syn;
+
+use proc_macro::TokenStream;
+
 mod derive;
 
 /// Marker trait that allows to tie in the procedural macro tool chain.
 trait Elm {}
+
+#[proc_macro_derive(Elm)]
+pub fn generate_elm(input: TokenStream) -> TokenStream {
+    let source = input.to_string();
+
+    let ast = syn::parse_derive_input(&source).unwrap();
+
+    derive::generate_elm_for(&ast);
+
+    input
+}
+
