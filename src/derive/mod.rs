44f1f889f89e787730b787fdf418fbc47d140092
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-29 09:22:39 +0100

    Write a test file with the name of the struct

--- src/derive/mod.rs
+++ src/derive/mod.rs
@@ -5,9 +5,10 @@ use std::fs::File;
 use std::io::{Write, BufWriter};
 use syn::DeriveInput;
 
-pub fn generate_elm(_: &DeriveInput) {
+pub fn generate_elm(ast: &DeriveInput) {
     let mut path = env::current_dir().unwrap();
-    path.push("generated/test.txt");
+    let name = &ast.ident;
+    path.push(format!("generated/{}.elm", name));
 
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
