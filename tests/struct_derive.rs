9867fdc42d9757d1176e393491c97c68e49381c8
Daan van Berkel <Daan.vanBerkel@han.nl>
2017-11-01 15:04:51 +0100

    Test content of generated file

--- tests/struct_derive.rs
+++ tests/struct_derive.rs
@@ -2,6 +2,9 @@
 extern crate serde_elm;
 
 use std::env;
+use std::fs::File;
+use std::io::{Read, Error};
+use std::path::Path;
 
 #[allow(dead_code)]
 #[derive(Elm)]
@@ -11,10 +14,30 @@ struct Simple {
 
 #[test]
 fn should_assert_elm_generation_was_done() {
-    let mut path = env::current_dir().unwrap();
+	let root = env::current_dir().unwrap();
+    let mut path = root.clone();
     path.push("generated");
     path.push("Simple.elm");
 
     assert!(path.exists());
     assert!(path.is_file());
+
+    let actual = contents_of(&path).unwrap();
+
+    let mut expected_path = root.clone();
+    expected_path.push("tests");
+    expected_path.push("assets");
+    expected_path.push("Simple.test.elm");
+
+    let expected = contents_of(&expected_path).unwrap();
+
+    assert_eq!(actual, expected);
+}
+
+fn contents_of(path: &Path) -> Result<String, Error> {
+    let mut file = File::open(path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+
+    Ok(content)
 }
