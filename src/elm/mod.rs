f4cc4ad308619af7f1dd06944857d29ff64be195
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-01 09:23:28 +0100

    Define definitions in a module

--- src/elm/mod.rs
+++ src/elm/mod.rs
@@ -10,13 +10,18 @@ impl Module {
     pub fn new<S>(name: S) -> Module where S: Into<String> {
         Module { name : name.into(), definitions: Vec::new() }
     }
+
+    pub fn define(&mut self, definition: Definition) {
+        self.definitions.push(definition);
+    }
 }
 
 impl Representation for Module {
     fn write_representation(&self, writer: &mut Write) -> Result<()> {
-        write!(writer, "module {} exposing (..)\n\n\n", self.name)?;
+        write!(writer, "module {} exposing (..)\n", self.name)?;
 
         for definition in &self.definitions {
+            write!(writer, "\n\n")?;
             definition.write_representation(writer)?
         }
 
@@ -25,24 +30,24 @@ impl Representation for Module {
 }
 
 pub enum Definition {
-    Struct,
-    Enum,
-    Function,
+    Record(String),
+    Enum(String),
+    Function(String),
 }
 
 impl Representation for Definition {
     fn write_representation(&self, writer: &mut Write) -> Result<()> {
         match *self {
-            Definition::Struct => {
-                write!(writer, "type alias")
+            Definition::Record(ref name) => {
+                write!(writer, "type alias {}", name)
             },
 
-            Definition::Enum => {
-                write!(writer, "type")
+            Definition::Enum(ref name) => {
+                write!(writer, "type {}", name)
             },
 
-            Definition::Function => {
-                write!(writer, "function")
+            Definition::Function(ref name) => {
+                write!(writer, "{} =", name)
             },
         }
     }
@@ -54,12 +59,45 @@ mod tests {
     use super::*;
 
     #[test]
-    fn module_should_write_it_self() {
-        let module = Module::new("Test");
+    fn empty_module_should_write_it_self() {
         let mut output = Vec::new();
+        let module = Module::new("Test");
 
         module.write_representation(&mut output).unwrap();
 
         assert_eq!(output[0..25], b"module Test exposing (..)"[..]);
+    }
+
+    #[test]
+    fn module_with_record_should_write_it_self() {
+        let mut output = Vec::new();
+        let mut module = Module::new("Test");
+        module.define(Definition::Record(String::from("TestRecord")));
+
+        module.write_representation(&mut output).unwrap();
+
+        assert_eq!(output[28..49], b"type alias TestRecord"[..]);
+    }
+
+    #[test]
+    fn module_with_enum_should_write_it_self() {
+        let mut output = Vec::new();
+        let mut module = Module::new("Test");
+        module.define(Definition::Enum(String::from("TestEnum")));
+
+        module.write_representation(&mut output).unwrap();
+
+        assert_eq!(output[28..41], b"type TestEnum"[..]);
+    }
+
+    #[test]
+    fn module_with_function_should_write_it_self() {
+        let mut output = Vec::new();
+        let mut module = Module::new("Test");
+        module.define(Definition::Function(String::from("TestFunction")));
+
+        module.write_representation(&mut output).unwrap();
+
+        assert_eq!(output[28..42], b"TestFunction ="[..]);
     }
 }
