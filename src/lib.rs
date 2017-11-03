9e504c85bc000b330deed854fc19f89be827becf
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-03 09:22:31 +0100

    Document libary

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,78 @@
+//! Provides easy communication between [Elm](http://elm-lang.org/) and
+//! [Rust](https://www.rust-lang.org/en-US/) by leveraging
+//! [serde](https://serde.rs/).
+//!
+//! ## Usage
+//! Notice that at the moment som of this is dreamcode.
+//!
+//! Lets say we have some models in Rust.
+//!
+//! ```rust
+//! enum Message {
+//!   FriendRequest(User),
+//!   Message(User, String),
+//! }
+//!
+//! struct User {
+//!   name: String
+//! }
+//! ```
+//!
+//! We want to generated the corresponding models in Elm. For this we need a
+//! dependency on the `serde_elm` crate. Include the following line in your
+//! `Cargo.toml`.
+//!
+//! ```toml
+//! [dependencies]
+//! serde_elm = "0.1.0"
+//! ```
+//!
+//! Next we need to make our project aware of the crate and the functionality it
+//! exposes. Import it in either `main.rs` or `lib.rs`. Don't forget to annotate the
+//! import with the `macro_use` annotation.
+//!
+//! ```rust
+//! #[macro_use]
+//! extern crate serde_elm;
+//! ```
+//!
+//! Now it is time to derive the corresponding models in Elm. Annotate the models
+//! with `derive(Elm)`.
+//!
+//! ```rust
+//! #[derive(Elm)]
+//! enum Message {
+//!   FriendRequest(User),
+//!   Message(User, String),
+//! }
+//!
+//! #[derive(Elm)]
+//! struct User {
+//!   name: String
+//! }
+//! ```
+//!
+//! Now every time your models change and you compile your code the corresponding
+//! Elm models will be generated. You can find them in the `generated` directory in
+//! your projects root. The are called after the corresponding Rust definitions.
+//! I.e. `Message.elm` and `User.elm` in this case.
+//!
+//! ```elm
+//! module Message exposing (..)
+//!
+//! type Message =
+//!     FriendRequest User
+//!   | Message User String
+//! ```
+//!
+//! ```elm
+//! module User exposing (..)
+//!
+//! type alias User =
+//!   {
+//!     name: String
+//!   }
+//! ```
 extern crate proc_macro;
 extern crate syn;
 
@@ -10,6 +85,8 @@ mod derive;
 /// Marker trait that allows to tie in the procedural macro tool chain.
 trait Elm {}
 
+/// Writes Elm model, serializers and deserializers to the `generated`
+/// directory.
 #[proc_macro_derive(Elm)]
 pub fn generate_elm(input: TokenStream) -> TokenStream {
     let source = input.to_string();
